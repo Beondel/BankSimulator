bb90335fc6e5eecf1a86ee5ebaa4696797e0eb9d
Beondel <bengmacmillan@gmail.com>
2017-01-05 18:47:25 -0800

    bank class updated

--- src/Bank.java
+++ src/Bank.java
@@ -2,4 +2,12 @@
  * Created by benjaminmacmillan1 on 1/5/17.
  */
 public class Bank {
+    public void transfer(Account giver, Account receiver, double amount) {
+        if (giver.balance > amount) {
+            giver.balance -= amount;
+            receiver.balance += amount;
+        } else {
+            System.out.println("Not enough funds in account");
+        }
+    }
 }
