7b20d1392c8ca27a2f3600f32ac4096ec9238aa2
siyukw <siyukw@gmail.com>
2017-01-05 18:46:09 -0800

    changed account

--- src/Account.java
+++ src/Account.java
@@ -4,4 +4,13 @@
 public class Account {
     // need an account number, password, balance
     // method: return balance
+    private int accountNum;
+    private String pswd;
+    double balance;
+
+    public Account(int accountNum, String pswd, double balance){
+        this.accountNum = accountNum;
+        this.pswd = pswd;
+        this.balance = balance;
+    }
 }
