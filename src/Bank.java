620939ab7491c3ae124988a570f7511df5302761
siyukw <siyukw@gmail.com>
2017-01-05 18:26:50 -0800

    main structure

--- src/Bank.java
+++ src/Bank.java
@@ -2,4 +2,9 @@
  * Created by benjaminmacmillan1 on 1/5/17.
  */
 public class Bank {
+    // transfer method - check balance, verify pswd, withdraw if there is more left
+
+    public static void main (String[] args){
+
+    }
 }
--- src/Client.java
+++ src/Client.java
@@ -2,4 +2,6 @@
  * Created by benjaminmacmillan1 on 1/5/17.
  */
 public class Client {
+    // need an account number, password, balance
+    // method: return balance
 }
