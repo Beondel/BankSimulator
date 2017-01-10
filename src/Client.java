7311e44d1594e76eef4dd239a65fce4ea71094eb
Beondel <bengmacmillan@gmail.com>
2017-01-10 13:50:17 -0800

    spelling

--- src/Client.java
+++ src/Client.java
@@ -11,7 +11,7 @@ public class Client {
         Account account2 = new Account(000002, "password12345", 35.00);
 
         System.out.println("giver account balance: " + account1.balance);
-        System.out.println("recevier account balance: " + account2.balance);
+        System.out.println("receiver account balance: " + account2.balance);
 
         kittenBank.transfer(account1, account2, 300000.00);
 
